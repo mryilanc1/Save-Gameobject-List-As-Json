d322e6630f80ec13880ca48f3515a569c011ebc0
Mükremin Ramazan YILANCI <41707639+mryilanc1@users.noreply.github.com>
2022-11-11 02:43:24 +0300

    Add files via upload

---
+++ Obje_List.cs
@@ -0,0 +1,8 @@
+
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Obje_List
+{
+    public List<Obje_Unit> Obje_list = new List<Obje_Unit>();
+}
---
+++ Obje_Unit.cs
@@ -0,0 +1,10 @@
+
+    [System.Serializable]
+    public class Obje_Unit
+    {
+        public  string Object_name;
+        public  string Collider_name;
+        public  string Layer_name;
+        public  string Materyal_name;
+        
+    }
