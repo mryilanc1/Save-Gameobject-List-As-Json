64ede45e337b8845748a7553c325baed1e51e6c6
Mükremin Ramazan YILANCI <41707639+mryilanc1@users.noreply.github.com>
2022-11-13 02:54:11 +0300

    object id

--- ObjectUnit.cs
+++ ObjectUnit.cs
@@ -3,7 +3,9 @@ namespace Editor
     [System.Serializable]
     public class ObjectUnit
     {
+        
         public string ObjectName;
+        public int ObjectID;
         public string ColliderName;
         public string LayerName;
         public string MaterialName;
