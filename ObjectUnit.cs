b05af4457e94a1fb874cf80e9b79346e6932c8e1
Mükremin Ramazan YILANCI <41707639+mryilanc1@users.noreply.github.com>
2022-11-13 04:14:08 +0300

    material unit added to objectunit

--- ObjectUnit.cs
+++ ObjectUnit.cs
@@ -1,13 +1,14 @@
+using System.Collections.Generic;
 namespace Editor
 {
     [System.Serializable]
     public class ObjectUnit
     {
-        
         public string ObjectName;
         public int ObjectID;
         public string ColliderName;
         public string LayerName;
-        public string MaterialName;
+        public List<MaterialUnit> Materials = new List<MaterialUnit>();
+        
     }
 }
